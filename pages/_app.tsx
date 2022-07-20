0bbdcbe51d0edd6a3e4a4afe5eab0f3a6b3bdced
Ahmed <ahmaat19@gmail.com>
2022-07-20 15:14:57 +0300

    initial

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react'
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import 'bootstrap/dist/css/bootstrap.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    typeof document !== undefined
+      ? require('bootstrap/dist/js/bootstrap')
+      : null
+  }, [])
+
+  return <Component {...pageProps} />
+}
+
+export default MyApp
