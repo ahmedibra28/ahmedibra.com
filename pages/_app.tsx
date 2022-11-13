92c839d9e52edb5c9571913606f78d8d6060a8d4
Ahmed <ahmaat19@gmail.com>
2022-11-13 08:27:05 +0300

    updating tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,9 +17,9 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=G-DTX3R04S4J`}
-        strategy='afterInteractive'
+        strategy='lazyOnload'
       />
-      <Script id='google-analytics' strategy='afterInteractive'>
+      <Script id='google-analytics' strategy='lazyOnload'>
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
@@ -29,7 +29,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         `}
       </Script>
 
-      <Script id='google-analytics' strategy='afterInteractive'>
+      <Script id='google-tag-manager' strategy='afterInteractive'>
         {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
